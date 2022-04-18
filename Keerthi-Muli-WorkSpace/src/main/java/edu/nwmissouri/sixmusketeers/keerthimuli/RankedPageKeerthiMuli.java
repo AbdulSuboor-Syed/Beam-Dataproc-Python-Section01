d341c9b31880ca0849d43acddd3d673b2deadf1c
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-04-18 12:54:36 -0500

    added argument constructor in RankPage

--- Keerthi-Muli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/keerthimuli/RankedPageKeerthiMuli.java
+++ Keerthi-Muli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/keerthimuli/RankedPageKeerthiMuli.java
@@ -1,5 +1,8 @@
 package edu.nwmissouri.sixmusketeers.keerthimuli;
 
+import java.util.ArrayList;
+
 public class RankedPageKeerthiMuli {
-    
+    public RankedPageKeerthiMuli(String key, ArrayList<VotingPageKeerthiMuli> voters) {
+    }
 }
