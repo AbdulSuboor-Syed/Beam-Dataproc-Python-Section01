22f6fb7d9fd3b994eebfb3b2ae6d7333aa8cc79b
vineetha-batchu <batchuvinetha@gmail.com>
2022-04-18 12:41:28 -0500

    Started Job2

---
+++ Vineetha-Batchu-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/vineethabatchu/RankedPage.java
@@ -0,0 +1,10 @@
+package edu.nwmissouri.sixmusketeers.vineethabatchu;
+
+import java.util.ArrayList;
+
+public class RankedPage {
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
+
+}
---
+++ Vineetha-Batchu-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/vineethabatchu/VotingPage.java
@@ -0,0 +1,8 @@
+package edu.nwmissouri.sixmusketeers.vineethabatchu;
+
+public class VotingPage {
+
+    public VotingPage(String voterName, Integer contributorVotes) {
+    }
+
+}
