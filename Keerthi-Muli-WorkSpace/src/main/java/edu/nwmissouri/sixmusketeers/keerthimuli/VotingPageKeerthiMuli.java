18c4adf76809a869b2a0950ade169e3165b4daa4
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-04-18 12:55:03 -0500

    added argument constructor in vote page

--- Keerthi-Muli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/keerthimuli/VotingPageKeerthiMuli.java
+++ Keerthi-Muli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/keerthimuli/VotingPageKeerthiMuli.java
@@ -1,5 +1,6 @@
 package edu.nwmissouri.sixmusketeers.keerthimuli;
 
 public class VotingPageKeerthiMuli {
-    
+    public VotingPageKeerthiMuli(String voterName, Integer contributorVotes) {
+    }
 }
