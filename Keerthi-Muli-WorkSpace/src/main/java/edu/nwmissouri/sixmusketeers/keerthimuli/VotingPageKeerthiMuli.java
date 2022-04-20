c126e1094e49c4b32ac6ded0abace600606a1815
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-04-19 22:09:46 -0500

    added getters and setters in voting page

--- Keerthi-Muli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/keerthimuli/VotingPageKeerthiMuli.java
+++ Keerthi-Muli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/keerthimuli/VotingPageKeerthiMuli.java
@@ -1,6 +1,22 @@
 package edu.nwmissouri.sixmusketeers.keerthimuli;
 
 public class VotingPageKeerthiMuli {
+    public String voterName;
+    public Integer contributorVotes;
     public VotingPageKeerthiMuli(String voterName, Integer contributorVotes) {
+        this.voterName = voterName;
+        this.contributorVotes = contributorVotes;
+    }
+    public String getVoterName(){
+        return voterName;
+    }
+    public  Integer getContributorVotes(){
+        return contributorVotes;
+    }
+    public void setVoterName(String voterName){
+        this.voterName = voterName;
+    }
+    public void setContributorVotes(Integer contributorVotes ){
+        this.contributorVotes = contributorVotes;
     }
 }
