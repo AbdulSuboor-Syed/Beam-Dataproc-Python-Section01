19b2f494e21ed1646768f837ea1e2269ef21b429
amulyamallepalli <77821601+amulyamallepalli@users.noreply.github.com>
2022-04-25 23:14:38 -0500

    Job2 Completed

--- Amulya-Mallepalli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/amulyamallepalli/RankedPage.java
+++ Amulya-Mallepalli-WorkSpace/src/main/java/edu/nwmissouri/sixmusketeers/amulyamallepalli/RankedPage.java
@@ -4,16 +4,46 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class RankedPage implements Serializable {
-    String key;
-    ArrayList<VotingPage> voterList = new ArrayList<>();
+    public String name;
+    public ArrayList<VotingPage> voters;
+    public Double rank;
+
+
+    public RankedPage(String voter,Double rank, ArrayList<VotingPage> voters){
+        this.name = voter;
+        this.voters = voters;
+        this.rank = rank;
+    }  
+
     public RankedPage(String key, ArrayList<VotingPage> voters) {
-        this.key = key;
-        this.voterList = voters;
+        this.name = key;
+        this.voters = voters;
+        this.rank = 1.0;
+    }
+
+    public String getKey(){
+        return name;
+    }
+
+    public  ArrayList<VotingPage> getVoters(){
+        return voters;
+    }
+
+    public void setKey(String key){
+        this.name = key;
+    }
+
+    public  void setVoters(ArrayList<VotingPage> voters){
+        this.voters = voters;
     }
     @Override
     public String toString() {
-        return "RankedPage [key=" + key + ", voterList=" + voterList + "]";
+        return String.format("%s,%.5f,%s", this.name,this.rank,this.voters.toString());
+       // return "RankedPage [name=" + name + ", voterList=" + voters + "rank= "+this.rank+"]";
     }
-    
+    public double getRank() {
+        return this.rank;
+    }
+
 
 }
